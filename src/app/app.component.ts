00e246bd8fff872e4d5b1f8284eaa270a1b0755f
achikovar <achikovar@gmail.com>
2024-04-30 11:43:07 +0400

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, ContentComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Fibonaci';
-
-  sumFibs(num: number) {
-    let n: number = 0;
-    let nPlusOne: number = 1;
-    let temp: number = 0;
-    let sum: number = 0;
-    while (nPlusOne <= num) {
-      temp = n;
-      n = nPlusOne;
-      nPlusOne = temp + n;
-      if (n % 2 !== 0) {
-        sum += n;
-      }
-    }
-    return sum;
-  }
-  ngOnInit(): void {
-    console.log(this.sumFibs(20));
-    //
-  }
+  title = 'Homework38';
 }
